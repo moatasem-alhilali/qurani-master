7933c46731d1352227ebe70ebaa2ab018b879b40
moatasem-alhilali <m.alhilalee@gmail.com>
2026-03-30 13:44:29 +0300

    add new data

--- android/app/src/main/kotlin/com/tamaneena/tamaneena_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/tamaneena/tamaneena_app/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.tamaneena.tamaneena_app
 
-import io.flutter.embedding.android.FlutterActivity
-import com.ryanheise.audioservice.AudioServiceActivity;
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : AudioServiceActivity()
