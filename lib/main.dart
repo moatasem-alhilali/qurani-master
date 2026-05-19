b36ff1cb0ff432279b778968cd8c78181e17c305
Moatasem Alhilali <m.alhilalee@gmail.com>
2026-05-19 17:10:10 +0300

    feat: update keystore configuration and change build mode to release

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
